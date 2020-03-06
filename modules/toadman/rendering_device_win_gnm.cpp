ebddb828045c6c76f070c16d22f18d82aa30c5dd
Ted Löfgren <ted.lofgren@thetoadman.com>
2020-03-06 09:27:35 +0100

    Match limit values for rendering device windows-gnm and gnm

--- modules/toadman/rendering_device_win_gnm.cpp
+++ modules/toadman/rendering_device_win_gnm.cpp
@@ -1,18 +1,22 @@
 #include "rendering_device_win_gnm.h"
 
 RenderingDeviceWinGNM::RenderingDeviceWinGNM() {
-
 }
 
 RenderingDeviceWinGNM::~RenderingDeviceWinGNM() {
-    
 }
 
 int RenderingDeviceWinGNM::limit_get(Limit p_limit) {
-    switch (p_limit) {
+	switch (p_limit) {
 		case LIMIT_MAX_TEXTURES_PER_SHADER_STAGE:
-			return 256;	// TEMP: This is to solve the current issue with to many gi probe textures in the shader.
+			return 128;
+		case LIMIT_MAX_UNIFORM_BUFFER_SIZE:
+			return 65536;
+		case LIMIT_MAX_COMPUTE_WORKGROUP_COUNT_X:
+		case LIMIT_MAX_COMPUTE_WORKGROUP_COUNT_Y:
+		case LIMIT_MAX_COMPUTE_WORKGROUP_COUNT_Z:
+			return 64;
 		default:
-			return RenderingDeviceVulkan::limit_get(p_limit);
+			ERR_FAIL_V(0);
 	}
 }
